180d51d6017b6511c4eb187a8097224b7b96fa7b
David <david.roszkowski@gmail.com>
2025-08-13 12:21:04 -0500

    Added check for command-line arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,9 +9,19 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is the sky blue?"
-    )
+
+    if len(sys.argv) < 2:
+        print("No prompt provided\nUsage: main.py 'prompt' ")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("Unknown arguments were provided\nUsage: main.py 'prompt'")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', 
+            contents=sys.argv[1]
+        )
+
     print(response.text)
     print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
     print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
